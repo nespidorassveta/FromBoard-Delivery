d772161cff1fb94f40a8fbe170c1cfcdeb765aca
nespidorassveta <gortepsix@gmail.com>
2024-07-21 01:39:06 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
